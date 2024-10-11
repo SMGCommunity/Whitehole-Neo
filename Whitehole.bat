8e8added850f3fd4d961200dd3c937a93d3f1c42
AwesomeTMC <38051573+AwesomeTMC@users.noreply.github.com>
2024-10-11 11:02:36 -0600

    Change --version to -version so it shows on older Java (Java 8)

--- Whitehole.bat
+++ Whitehole.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Running Whitehole using Java Version (Java 11 or newer REQUIRED):
-java --version
+java -version
 java --add-exports=java.desktop/sun.awt=ALL-UNNAMED -Dsun.java2d.uiScale=1.0 -jar Whitehole.jar
 if %errorlevel% GEQ 1 (
     echo Whitehole exited with error code %errorlevel%
