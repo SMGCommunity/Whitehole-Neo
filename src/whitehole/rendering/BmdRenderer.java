a834bdf1fd8956224dffac4ff8862231f7a56729
Super Hackio <superhaxio@gmail.com>
2026-05-20 09:25:53 -0700

    unify color and alpha inputs

--- src/whitehole/rendering/BmdRenderer.java
+++ src/whitehole/rendering/BmdRenderer.java
@@ -566,24 +566,24 @@ public class BmdRenderer extends GLRenderer {
             "gl_Color"                // TEXGENSRC_COLOR1
         };
         String[] outputregs = { "rprev", "r0", "r1", "r2" };
+
         String[] c_inputregs = { 
-            "truncc3(rprev.rgb)",
-            "truncc3(rprev.aaa)",
-            "truncc3(r0.rgb)",
-            "truncc3(r0.aaa)",
-            "truncc3(r1.rgb)",
-            "truncc3(r1.aaa)",
-            "truncc3(r2.rgb)",
-            "truncc3(r2.aaa)",
-            "texcolor.rgb", "texcolor.aaa", 
-            "rascolor.rgb", "rascolor.aaa",
+            "rprev.rgb",
+            "rprev.aaa",
+            "r0.rgb",
+            "r0.aaa",
+            "r1.rgb",
+            "r1.aaa",
+            "r2.rgb",
+            "r2.aaa",
+            "texcolor.rgb",
+            "texcolor.aaa", 
+            "rascolor.rgb",
+            "rascolor.aaa",
             "vec3(1.0,1.0,1.0)",
             "vec3(0.5,0.5,0.5)",
             "konst.rgb",
             "vec3(0.0,0.0,0.0)" };
-        String[] c_inputregsD = { 
-            "rprev.rgb", "rprev.aaa", "r0.rgb", "r0.aaa", "r1.rgb", "r1.aaa", "r2.rgb", "r2.aaa", "texcolor.rgb", "texcolor.aaa", 
-            "rascolor.rgb", "rascolor.aaa", "vec3(1.0,1.0,1.0)", "vec3(0.5,0.5,0.5)", "konst.rgb", "vec3(0.0,0.0,0.0)" };
         String[] c_konstsel = { 
             "vec3(1.0,1.0,1.0)", "vec3(0.875,0.875,0.875)", "vec3(0.75,0.75,0.75)", "vec3(0.625,0.625,0.625)", "vec3(0.5,0.5,0.5)", "vec3(0.375,0.375,0.375)", "vec3(0.25,0.25,0.25)", "vec3(0.125,0.125,0.125)", "", "", 
             "", "", "k0.rgb", "k1.rgb", "k2.rgb", "k3.rgb", "k0.rrr", "k1.rrr", "k2.rrr", "k3.rrr", 
@@ -869,7 +869,7 @@ public class BmdRenderer extends GLRenderer {
             a = c_inputregs[tv.combineColorA];
             b = c_inputregs[tv.combineColorB];
             c = c_inputregs[tv.combineColorC];
-            d = c_inputregsD[tv.combineColorD];
+            d = c_inputregs[tv.combineColorD];
 
             switch(tv.operationColor)
             {
